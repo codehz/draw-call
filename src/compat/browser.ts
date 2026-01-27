3d612c7e5385bc382fb30ec021267b2a630f1ab9
codehz <git@hertz.moe>
2026-01-27 23:56:48 +0800

    fix(compat): handle undefined window for DOMMatrix and Path2D

--- src/compat/browser.ts
+++ src/compat/browser.ts
@@ -1,5 +1,5 @@
-export const DOMMatrix = window.DOMMatrix;
-export const Path2D = window.Path2D;
+export const DOMMatrix = typeof window !== "undefined" ? window.DOMMatrix : undefined;
+export const Path2D = typeof window !== "undefined" ? window.Path2D : undefined;
 
 export function createRawCanvas(width: number, height: number) {
   const canvas = document.createElement("canvas");
